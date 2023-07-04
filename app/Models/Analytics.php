17e8ca439ab5880c2a71cea1c73069779ab33124
unknown <qomaruddin5@gmail.com>
2023-07-04 23:06:02 +0700

    first commit

---
+++ app/Models/Analytics.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Analytics extends Model
+{
+    use HasFactory;
+
+    protected $table = 'analytics';
+
+    public $timestamps = true; //by default timestamp false
+
+    protected $fillable = ['analytics','ip'];
+}
